4cb37c0cd18d3287d6f1233a45e92b6d5d07632f
Le0nardoGrandi <leonardo.falso01@gmail.com>
2020-11-10 12:19:16 +0100

    second commit

---
+++ esonero.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+void inputk(char M[],char *k){ //dichiarazione della funzione per l'inserimento manuale della chiave
+	printf("Inserisci la chiave di cifratura: "); //richiesta di inserimento della chiave
+	do{
+		fgets(k, 129, stdin); //inserimento della chiave
+	}while(strlen(k)<strlen(M)); //do-while per fa rinserire la chiave finchè non sia più corta del messaggio in chiaro
+}
+void randomk(char M[],char *k){ //dichiarazione della funzione per la generazione automatica della chiave
+	time_t t; //inizializzazione del generatore di numeri casuali utilizzando il tempo attuale
+	srand((unsigned)time(&t)); //chiamata alla funzione per la generazione di numeri casuali
+	for(int c=0; c<(strlen(M)-1); c++){ //ciclo che per far scorrere la stringa, carattere per carattere
+		k[c]=rand()%(127)+32; //inserimento di valori casuali nella stringa
+	}
+	printf("Chiave generata automaticamente: \n%s\n", k); //output della chiave generata
+}
+void criptapazione(char M[],char *k, char *C){ //dichiarazione della funzione per la criptazione del messaggio
+	for(int i=0; i<(strlen(M)-1);i++){ //ciclo che per far scorrere la stringa, carattere per carattere
+		C[i]=M[i]^k[i]; //criptazione del messaggio
+	}
+	printf("Messaggio criptato: \n%s\n", C); //output del messaggio criptato
+}
+void decriptapazione(char *M,char *k, char *C){ //dichiarazione della funzione per la decriptazione del messaggio
+	for(int i=0; i<(strlen(M)-1);i++){ //ciclo che per far scorrere la stringa, carattere per carattere
+		M[i]=C[i]^k[i]; //criptazione del messaggio
+	}
+	printf("Messaggio decriptato: \n%s\n", M); //output del messaggio decriptato
+}
+int main()
+{
+	char M[129],k[129],C[129]; //dichiarazione delle stringhe: messaggio in chiaro, chiave di criptazione, messaggio criptato
+	int n; //dichiarazione della variabile utilizzata per il menù
+	printf("Inserisci il messaggio: "); //richiesta di inserimento del messaggio in chiaro
+	fgets(M, 129, stdin); //input del messaggio in chiaro
+	do{
+		printf("Inserisci: \n1 per inserire la chiave di cifratura manualmente \n2 per generare casualmente la chiave\n"); //richiesta di inserimento per il menù
+
+		scanf ("%d", &n); //input per la scelta di come inserire la chiave
+	}while(n!=1&&n!=2); // do-while per fa rinserire il valore talvolta sia diverso da 1 o 2
+	if(n==1){
+		inputk(M,k); //chiamata alla funzione per inserire la chiave manualmente
+	}
+	if(n==2){
+		randomk(M,k); //chiamata alla funzione per generare la chiave automaticamente
+	}
+	criptapazione(M,k,C); //chiamata alla funzione per la criptazione del messaggio
+	decriptapazione(M,k,C); //chiamata alla funzione per la decriptazione del messaggio
+	return 0;
+}
